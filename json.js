f74e1e9d3a5040205d495a1082f7dad0e5b15848
Manojthen <152765463+Manojthen@users.noreply.github.com>
2024-01-01 05:18:39 -0800

    Add files via upload

---
+++ json.js
@@ -0,0 +1,49 @@
+
+    "basic"
+    [
+     {
+      "name": "Your first and last name",
+      "picture": "",
+      "email": "Your email address",
+      "phone": "A phone number, with any formatting you like. E.g. (555) 555-5555.",
+      "degree": "",
+      "website": "Your website URL",
+      "summary":"A one-sentence to one-paragraph overview text. Do not include any line-breaks."
+    
+      }
+    ] 
+      
+      "location"; 
+      [
+        {
+         "address":"Your street address or mailing address",
+         "postalCode": "Your postal code (ZIP in the U.S.)",
+         "city": "Your city",
+         "countryCode": "Your country (e.g. USA)",
+         "region":"Your region (state in the U.S.)"
+        }
+      ]
+    
+      "profiles";
+       [
+        {
+          "network": "A social media or other profile that you would like to include (e.g. LinkedIn, Twitter)",
+          "username": "Your username on this network",
+          "url": "A URL to your user profile page"
+        }
+      ]
+    
+    "work"; [
+      {
+        "company": "Your employer name",
+        "position": "Your job title",
+        "website": "The URL for the employer's website",
+        "startDate": "Your start date, in YYYY-MM-DD format",
+        "endDate": "Your end date, in YYY-MM-DD format (leave blank for a current position)",
+        "summary": "A one-sentence to one-paragraph summary of this employer or position",
+        "highlights": [
+          "Bullet-point list items that you would like to include along with (or instead of) a summary paragraph."
+        ]
+      }
+    ]
+    
